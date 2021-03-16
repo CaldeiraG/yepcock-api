3c72ff7d58780d182a26978d112d6b32e48e2a15
CaldeiraG <tiagcaldeira@gmail.com>
2021-03-16 21:53:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const client = new tmi.Client({
 client.connect()
 
 client.on('message', (channel, tags, message, self) => {
-  if (message.includes('YEP COCK')) {
-    counter++
+  if ((message.match(/YEP COCK/g) || []).length > 0) {
+    counter = counter + (message.match(/YEP COCK/g) || []).length
     console.log(`${tags['display-name']}: ${message} (${counter})`)
   }
 })
